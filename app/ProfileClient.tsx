90a191f61319cf33cdbcf6bc7560b3c5ee3f7163
Whitey <c1783468@gmail.com>
2023-12-27 00:03:20 +0800

    add auth0 login & logout feature

---
+++ app/ProfileClient.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { useUser } from '@auth0/nextjs-auth0/client';
+
+export default function ProfileClient() {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>{error.message}</div>;
+
+  if (!user) return <span>Not logged in</span>;
+
+  return (
+    user && (
+      <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <div className="flex justify-end px-4 pt-4"></div>
+        <div className="flex flex-col items-center pb-10">
+          <img
+            className="w-24 h-24 mb-3 rounded-full shadow-lg"
+            src={user.picture}
+            alt={user.name}
+          />
+          <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+            {user.name}
+          </h5>
+          <span className="text-sm text-gray-500 dark:text-gray-400">
+            {user.email}
+          </span>
+        </div>
+      </div>
+    )
+  );
+}
