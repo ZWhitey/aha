7d512575f84fa4cd9bd026a2fe0671aec47ef7f0
Whitey <c1783468@gmail.com>
2023-12-30 14:00:15 +0800

    fix nextjs load image failed

--- app/ProfileClient.tsx
+++ app/ProfileClient.tsx
@@ -20,6 +20,8 @@ export default function ProfileClient() {
             className="w-24 h-24 mb-3 rounded-full shadow-lg"
             src={user.picture ?? ''}
             alt={user.name ?? 'user picture'}
+            width={100}
+            height={100}
           />
           <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
             {user.name}
