2085e36e4edf15ec3061925e8b94efce6095cd64
Whitey <c1783468@gmail.com>
2023-12-30 13:08:20 +0800

    fix compile error

--- app/ProfileClient.tsx
+++ app/ProfileClient.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useUser } from '@auth0/nextjs-auth0/client';
+import Image from 'next/image';
 
 export default function ProfileClient() {
   const { user, error, isLoading } = useUser();
@@ -15,10 +16,10 @@ export default function ProfileClient() {
       <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <div className="flex justify-end px-4 pt-4"></div>
         <div className="flex flex-col items-center pb-10">
-          <img
+          <Image
             className="w-24 h-24 mb-3 rounded-full shadow-lg"
-            src={user.picture}
-            alt={user.name}
+            src={user.picture ?? ''}
+            alt={user.name ?? 'user picture'}
           />
           <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
             {user.name}
